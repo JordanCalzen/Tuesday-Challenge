df0ee3e875a2ecf3f5efb2bc13e2b70036ca614b
JordanCalzen <jordancarlzencare@gmail.com>
2024-12-03 15:16:27 -0800

    aded new files
    added sass partials

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-
+const sales = [
+	{
+		id: 1,
+		pdtName: "Vegetables",
+		image: "./imgs/carrot-removebg-preview.png",
+		price: 20,
+	},
+	{
+		id: 2,
+		pdtName: "Chicken",
+		image: "./imgs/carrot-removebg-preview.png",
+		price: 30,
+	},
+	{
+		id: 3,
+		pdtName: "Milk",
+		image: "./imgs/carrot-removebg-preview.png",
+		price: 25,
+	},
+	{
+		id: 4,
+		pdtName: "Snacks",
+		image: "./imgs/carrot-removebg-preview.png",
+		price: 15,
+	},
+];
