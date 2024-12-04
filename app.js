c8e3a804cece5de955c9719045b8789298a1bdb6
JordanCalzen <jordancarlzencare@gmail.com>
2024-12-03 17:53:01 -0800

    added some js scripts
    removed some innerHtml

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const ourSales = document.querySelector(".salespdts");
+
 const sales = [
 	{
 		id: 1,
@@ -23,4 +25,46 @@ const sales = [
 		image: "./imgs/carrot-removebg-preview.png",
 		price: 15,
 	},
+	{
+		id: 5,
+		pdtName: "Snacks",
+		image: "./imgs/carrot-removebg-preview.png",
+		price: 15,
+	},
+	{
+		id: 6,
+		pdtName: "Snacks",
+		image: "./imgs/carrot-removebg-preview.png",
+		price: 15,
+	},
+	{
+		id: 7,
+		pdtName: "Snacks",
+		image: "./imgs/carrot-removebg-preview.png",
+		price: 15,
+	},
+	{
+		id: 8,
+		pdtName: "Snacks",
+		image: "./imgs/carrot-removebg-preview.png",
+		price: 15,
+	},
 ];
+
+sales.forEach((item) => {
+	const product = document.createElement("div");
+	product.classList.add("prodct");
+	product.innerHTML = `
+        
+			<div class="pdt">
+					<img src="${item.image}" width="30"
+						height="30" alt="" />
+					<h3>${item.pdtName}<span>(Local Shop)</span></h3>
+					<p>500gm</p>
+					<h1>${item.price}.<sup>29$</sup></h1>
+					<button class="add-to-cart">+</button>
+			</div>
+    `;
+	ourSales.appendChild(product);
+	console.log(ourSales);
+});
